c96a3625b8e8e2d99990d6e6c796e9b50888d7d5
Story81 <story81@bk.ru>
2023-10-24 14:32:47 +0400

    Исправлен класс DriverManager. Перегружен chromedriver.exe

--- src/main/java/org/example/project/DriverManager.java
+++ src/main/java/org/example/project/DriverManager.java
@@ -23,8 +23,9 @@ public class DriverManager {
     }
 
     public static void initDriver() {
-        driver = new ChromeDriver();
         setProperty(properties.getProperty("WEB_DRIVER"), properties.getProperty("WEB_DRIVER_PATH"));
+        driver = new ChromeDriver();
+
         driver.get(properties.getProperty("HOSTNAME"));
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         driver.manage().window().maximize();
